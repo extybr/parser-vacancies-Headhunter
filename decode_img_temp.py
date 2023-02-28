b759063aea962a406b3aa946f4f4c9365cf11cf3
Andrey Plotnikov <plo-av@protonmail.com>
2023-03-01 02:17:26 +1000

    fix decode_img_temp

--- decode_img_temp.py
+++ decode_img_temp.py
@@ -9,17 +9,20 @@ def decode_b64() -> tuple:
     """ Переменные окружения.
     Декодирование файла из base64.
     Сохранение иконки """
-    pic1 = 'hh.svg'
-    pic2 = 'trudvsem.png'
+    pic_hh = 'hh.svg'
+    pic_tv = 'trudvsem.png'
     try:
-        path1_temp = str(Path(gettempdir()) / pic1)
-        path2_temp = str(Path(gettempdir()) / pic2)
-        dec1 = decodebytes(hh.encode())
-        dec2 = decodebytes(tv.encode())
-        with open(path1_temp, 'wb') as file:
-            file.write(dec1)
-        with open(path2_temp, 'wb') as file:
-            file.write(dec2)
-        return path1_temp, path2_temp
+        path_hh_temp = Path(gettempdir()) / pic_hh
+        path_tv_temp = Path(gettempdir()) / pic_tv
+        if Path.exists(path_hh_temp) and Path.exists(path_tv_temp):
+            return str(path_hh_temp), str(path_tv_temp)
+        else:
+            dec1 = decodebytes(hh.encode())
+            dec2 = decodebytes(tv.encode())
+            with open(path_hh_temp, 'wb') as file:
+                file.write(dec1)
+            with open(path_tv_temp, 'wb') as file:
+                file.write(dec2)
+            return str(path_hh_temp), str(path_tv_temp)
     except Exception:
-        return pic1, pic1
+        return 'favicon.ico', 'favicon.ico'
